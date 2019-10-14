85871e2ef401cf90db5ce7e0b4d111ab37bdde91
chihying.lin <chihying.lin@latticeworkinc.com>
2019-10-14 19:40:20 +0800

    V0.2

--- src/better_thread.py
+++ src/better_thread.py
@@ -6,31 +6,82 @@ import logging
 
 # A "BETTER" thread which support start(timeout=xxx) and terminate()
 class BThread(threading.Thread):
+    LOG_FMT = "%(asctime)s [%(levelname)s] " \
+              "%(filename)s:%(lineno)s %(name)s %(funcName)s() : %(message)s"
+
+    def _process_timeout(self, timeout=None):
+        ret = 0
+
+        if timeout:
+            try:
+                timeout = float(timeout)
+                if timeout >= 0:
+                    ret = timeout
+                else:
+                    raise ValueError('less than 0')
+            except ValueError as e:
+                self._log.error(e)
+                raise ValueError('not a float')
+
+        return ret
+
+    # timeout(float, >=0):
+    #    0: no timeout
+    # exception:
+    #    ValueError: invalid timeout
     def __init__(self, *args, **kwargs):
         # 'timeout' is not for original thread
-        self.timeout = None
+        self.default_timeout = None
         if 'timeout' in kwargs:
-            self.timeout = kwargs['timeout']
-            kwargs.remove('timeout')
+            self.default_timeout = self._process_timeout(kwargs['timeout'])
+            del kwargs['timeout']
 
         super(BThread, self).__init__(*args, **kwargs)
 
         self.setName(self.name + '_GThread_' + time.strftime("%Y%m%d-%H%M%S"))
+
+        logging.basicConfig(level=logging.WARNING, format=self.LOG_FMT)
         self._log = logging.getLogger(self.getName())
 
+        # terminate this thread when sys.exit()
+        # self.setDaemon(True)
+
+    # level: python logging's level
+    def dbg(self, level=logging.DEBUG):
+        self._log.setLevel(level)
+
+    # timeout(float, >=0):
+    #    0: no timeout
+    # exception:
+    #    ValueError: invalid timeout
+    def set_timeout(self, timeout):
+        self.default_timeout = self._process_timeout(timeout)
+
+    # timeout(float, >=0):
+    #    0: no timeout
+    # exception:
+    #    ValueError: invalid timeout
     def start(self, timeout=None):
-        current_timeout = timeout if timeout else self.timeout
-        if current_timeout:
+
+        if timeout or self.default_timeout:
+            if not timeout:
+                timeout = self.default_timeout
+
+            self._log.info("timeout=%s", timeout)
+
             # create another thread to terminate self if timeout
-            threading.Thread(target=self._terminate_thread,
-                             args=(self, current_timeout)).start()
+            t = threading.Thread(target=self._terminator,
+                                 args=(self, timeout))
+            t.start()
 
         super(BThread, self).start()
 
-    @staticmethod
-    def _terminate_thread(thread, timeout):
+    def _terminator(self, thread, timeout):
+        self._log.debug("%s's terminator", thread.name)
+
         time.sleep(timeout)
-        if thread.is_alive():
+        if thread and thread.is_alive():
+            self._log.info("terminate %s", thread.name)
             thread.terminate()
 
     # ret:
@@ -38,7 +89,8 @@ class BThread(threading.Thread):
     #   False: fail
     def terminate(self):
         if self.is_alive():
-            self._log.info('terminate thread <%s> tid=%s', self.name, self.ident)
+            self._log.info('terminate thread <%s> tid=%s',
+                           self.name, self.ident)
 
             res = ctypes.pythonapi.PyThreadState_SetAsyncExc(self.ident,
                                                              ctypes.py_object(
@@ -54,36 +106,62 @@ class BThread(threading.Thread):
 ########
 # test #
 ########
-LOG_FMT = "%(asctime)s [%(levelname)s] " \
-          "%(filename)s:%(lineno)s %(name)s %(funcName)s() : %(message)s"
-logging.basicConfig(level=logging.DEBUG, format=LOG_FMT)
-
-
-def infinite_loop():
+def infinite_loop(name):
+    interval = 0.1
+    i = 1
     while True:
-        print('infinite_loop...')
-        time.sleep(1)
+        print('{}: infinite_loop...{}'.format(name, i))
+        i += interval
+        time.sleep(interval)
 
 
-def test1():
-    t1 = BThread(target=infinite_loop)
+def test_terminate_thread_asyn():
+    print("Terminate thread asynchronously")
+    t = BThread(target=infinite_loop, args=('test_terminate_thread_asyn',))
+    t.dbg(logging.ERROR)
 
-    t1.start()
+    t.start()
 
     time.sleep(2)
+    print("TERMINATE")
+    t.terminate()
 
-    print('TERMINATE')
-    t1.terminate()
-
-    print('JOIN')
-    t1.join()
+    t.join()
 
 
-def test2():
-    t2 = BThread(target=infinite_loop)
-    t2.start(timeout=3)
-    t2.join()
+def test_start_with_timeout():
+    print("Start thread with timeout 3s")
+    t = BThread(target=infinite_loop, args=('test_start_timeout',))
+    t.dbg(logging.DEBUG)
+
+    t.start(timeout=3)
+
+    t.join()
+
+
+def test_set_timeout1():
+    print("Set timeout #1 3s")
+    t = BThread(target=infinite_loop, args=('test_set_timeout1',), timeout=3)
+    t.dbg(logging.DEBUG)
+
+    t.start()
+
+    t.join()
+
+
+def test_set_timeout2():
+    print("Set timeout #2 3s")
+    t = BThread(target=infinite_loop, args=('test_set_timeout2',))
+    t.dbg(logging.DEBUG)
+    t.set_timeout(3)
+
+    t.start()
+
+    t.join()
 
 
 if __name__ == '__main__':
-    test2()
+    test_terminate_thread_asyn()
+    # test_start_with_timeout()
+    # test_set_timeout1()
+    # test_set_timeout2()
